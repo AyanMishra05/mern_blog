ab33ec50c0e395b6fcb9533bdb1415862d8df4da
Ayan Mishra <ayanmishra2021@gmail.com>
2026-02-11 20:28:57 +0530

    Created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -25,3 +26,4 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);

6a2b6d140cfee668a9b5dfa929f87957ceef26bc
Ayan Mishra <ayanmishra2021@gmail.com>
2026-02-11 23:02:17 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
@@ -25,3 +27,33 @@ export const signup = async (req, res, next) => {
     next(err);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email.trim() === "" || password.trim() === "") {
+    next(errorHandler(400, "All field are required"));
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return next(errorHandler(400, "Invalid credentials"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = user._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error.message);
+  }
+};
+ 
